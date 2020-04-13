95b5b7a27c748a80f89b9bc96118cb0d6dd2a0e9
Sophie <sophie.hm@protonmail.com>
2020-04-14 01:57:01 +0200

    Parametres fonctionnels
    - Permet de modifier les infos de la bibliothèque (nom, type & adresse)
    - Gestion des auteurs, genres et editeurs

---
+++ source/settings.cpp
@@ -0,0 +1,93 @@
+#include "settings.h"
+#include "ui_settings.h"
+
+settings::settings(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::settings)
+{
+    ui->setupUi(this);
+
+    fillForm();
+}
+
+settings::~settings()
+{
+    delete ui;
+}
+
+void settings::fillForm(){
+    QSqlQuery query;
+    query.exec("SELECT * FROM settings");
+    query.last();
+    ui->libraryNameLineEdit->setText(query.value(1).toString());
+    ui->addressLineEdit->setText(query.value(2).toString());
+    ui->typeComboBox->setLineEdit(new QLineEdit(query.value(3).toString()));
+    ui->changeNewsTextEdit->setPlainText(query.value(4).toString());
+}
+
+void settings::fillTable(const QString &selected){
+    QString type=selected;
+    if(selection.indexOf("_")==-1){
+        type.prepend("b_");
+    }
+    selection = selected;
+
+    QSqlQuery query;
+    query.exec("SELECT COUNT(name) FROM "+type);
+    query.first();
+    ui->tableWidget->setRowCount(query.value(0).toInt());
+
+    int row=0;
+    query.exec("SELECT name FROM "+type);
+    while(query.next()){
+        ui->tableWidget->setItem(row, 0, new QTableWidgetItem(query.value(0).toString()));
+        ++row;
+    }
+}
+
+void settings::on_saveButton_clicked()
+{
+    qDebug() << ui->typeComboBox->currentIndex();
+    if(!((ui->libraryNameLineEdit->text().isEmpty() && ui->addressLineEdit->text().isEmpty()) || ui->typeComboBox->currentIndex()==0)){
+        QSqlQuery update;
+        update.prepare("INSERT INTO settings VALUES(current_timestamp(), :name, :address, :type, :message)");
+        update.bindValue(":name", ui->libraryNameLineEdit->text());
+        update.bindValue(":address", ui->addressLineEdit->text());
+        update.bindValue(":type", ui->typeComboBox->currentText());
+        update.bindValue(":message", ui->changeNewsTextEdit->toPlainText());
+        if(update.exec()){
+            QMessageBox::information(this, "Success!", "Settings have been saved!");
+        }
+        else {
+            QMessageBox::critical(this, "Error...", "Data couldn't be saved. Retry later.");
+        }
+    }
+    else QMessageBox::critical(this, "Error!", "Please fill all the fields");
+
+}
+
+void settings::on_comboBox_currentIndexChanged(const QString &selected)
+{
+    fillTable(selected);
+}
+
+void settings::keyPressEvent(QKeyEvent *event){
+    if(event->key()==Qt::Key_Delete){
+        int row = ui->tableWidget->currentRow();
+        QString name = ui->tableWidget->item(row, 0)->text();
+        int answer = QMessageBox::warning(this, "Delete?", "Are you sure to delete "+name+"?\nNote that you can only delete a name who isn't used.", QMessageBox::Yes | QMessageBox::No);
+        if(answer == QMessageBox::Yes){
+            QString table = selection;
+            table.prepend("b_");
+            QSqlQuery deleteBook("DELETE FROM "+table+" WHERE name = '"+name+"'");
+            if(deleteBook.exec()){
+                QMessageBox::information(this, "Success!", name+" has been successfully deleted!");
+                ui->tableWidget->clearContents();
+                fillTable(selection);
+            }
+            else{
+                QMessageBox::critical(this, "Error", "Cannot delete "+name+", delete the related books and try again.");
+            }
+        }
+    }
+}
---
+++ source/settings.h
@@ -0,0 +1,36 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QWidget>
+#include <QtSql>
+#include <QMessageBox>
+
+namespace Ui {
+class settings;
+}
+
+class settings : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit settings(QWidget *parent = nullptr);
+    ~settings();
+
+    void fillForm();
+    void fillTable(const QString &selected);
+
+    void keyPressEvent(QKeyEvent *event);
+
+private slots:
+    void on_saveButton_clicked();
+
+    void on_comboBox_currentIndexChanged(const QString &arg1);
+
+private:
+    Ui::settings *ui;
+
+    QString selection;
+};
+
+#endif // SETTINGS_H
