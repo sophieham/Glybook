39a10f22e27a30f9fa98a9ebbb7f192e6928ece2
Sophie <sophie.hm@protonmail.com>
2020-10-06 12:30:19 +0200

    Version executable
    Pour obtenir le logiciel facilement!

---
+++ exe/CMakeFiles/Glybook.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Glybook_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Glybook_autogen.dir/ParseCache.txt"
+  "Glybook_autogen"
+  "CMakeFiles/Glybook.dir/Glybook_autogen/EWIEGA46WW/qrc_resource.cpp.obj"
+  "CMakeFiles/Glybook.dir/Glybook_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/Glybook.dir/accountdialog.cpp.obj"
+  "CMakeFiles/Glybook.dir/accounthistory.cpp.obj"
+  "CMakeFiles/Glybook.dir/book.cpp.obj"
+  "CMakeFiles/Glybook.dir/bookdialog.cpp.obj"
+  "CMakeFiles/Glybook.dir/bookinformation.cpp.obj"
+  "CMakeFiles/Glybook.dir/catalog.cpp.obj"
+  "CMakeFiles/Glybook.dir/connection.cpp.obj"
+  "CMakeFiles/Glybook.dir/cryptohashclass.cpp.obj"
+  "CMakeFiles/Glybook.dir/dbconnection.cpp.obj"
+  "CMakeFiles/Glybook.dir/genre.cpp.obj"
+  "CMakeFiles/Glybook.dir/glybook.cpp.obj"
+  "CMakeFiles/Glybook.dir/initialization.cpp.obj"
+  "CMakeFiles/Glybook.dir/library.cpp.obj"
+  "CMakeFiles/Glybook.dir/loandialog.cpp.obj"
+  "CMakeFiles/Glybook.dir/main.cpp.obj"
+  "CMakeFiles/Glybook.dir/manageaccounts.cpp.obj"
+  "CMakeFiles/Glybook.dir/myaccount.cpp.obj"
+  "CMakeFiles/Glybook.dir/reservation.cpp.obj"
+  "CMakeFiles/Glybook.dir/settings.cpp.obj"
+  "CMakeFiles/Glybook.dir/statistics.cpp.obj"
+  "CMakeFiles/Glybook.dir/user.cpp.obj"
+  "Glybook.exe"
+  "Glybook.exe.manifest"
+  "Glybook.pdb"
+  "Glybook_autogen/EWIEGA46WW/qrc_resource.cpp"
+  "libGlybook.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Glybook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exe/CMakeFiles/Glybook_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Glybook_autogen"
+  "Glybook_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Glybook_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
